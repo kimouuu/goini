66dcecf2aa95a467e7df432806d1262bbfb5e6b8
kimouya <milanabila53@gmail.com>
2023-09-19 13:24:46 +0700

    baru

--- configs/env.go
+++ configs/env.go
@@ -1,7 +1,6 @@
 package configs
 
 import (
-
  "log"
  "os"
 
--- database/mongodb.go
+++ database/mongodb.go
@@ -4,6 +4,7 @@ import (
     "context"
     "log"
 
+    
     "go.mongodb.org/mongo-driver/mongo"
     "go.mongodb.org/mongo-driver/mongo/options"
     "go.mongodb.org/mongo-driver/bson"
